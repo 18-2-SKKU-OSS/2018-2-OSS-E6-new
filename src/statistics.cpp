7004186c823253d9670bb8473100a610b4c34d0e
yjh1534 <dbswogks1534@naver.com>
2018-12-06 17:39:13 +0900

    Don't have to make statistics.txt by yourself to handle trash value

--- src/statistics.cpp
+++ src/statistics.cpp
@@ -5,6 +5,11 @@ bool Stats::collectStatistics(int size) {
   std::string file_dir = "../data/statistics" + std::to_string(size) + ".txt";
   std::fstream statistics(file_dir);
   if (statistics.fail()) {
+    bestScore = 0;
+    gameCount = 0;
+    winCount = 0;
+    totalMoveCount = 0;
+    totalDuration = 0;
     return false;
   }
 
