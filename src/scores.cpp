e49b2137ee6a66124bb585c7057572f69af69ea9
yjh1534 <dbswogks1534@naver.com>
2018-12-10 13:33:13 +0900

    translate scripts of game to Korean

--- src/scores.cpp
+++ src/scores.cpp
@@ -8,11 +8,19 @@ bool compare(const Score &a, const Score &b) {
 
 void Scoreboard::prompt() {
   constexpr auto score_prompt_text =
-      "Please enter your name to save this score: ";
+  "Please enter your name to save this score: ";
+  constexpr auto score_prompt_text_kor =
+  "점수를 저장할 이름을 입력해주십시오: ";
   constexpr auto sp = "  ";
 
   std::ostringstream score_prompt_richtext;
-  score_prompt_richtext << bold_on << sp << score_prompt_text << bold_off;
+  if(language==1){
+    score_prompt_richtext << bold_on << sp << score_prompt_text << bold_off;
+  }
+
+  else{
+    score_prompt_richtext << bold_on << sp << score_prompt_text_kor << bold_off;
+  }
 
   std::cout << score_prompt_richtext.str();
   std::cin >> name;
@@ -22,141 +30,277 @@ void Scoreboard::writeToFile() {
   std::string file_dir = "../data/scores"+ std::to_string(playsize) + ".txt";
   std::fstream scores(file_dir, std::ios_base::app);
   scores << std::endl
-         << name << " " << score << " " << win << " " << moveCount << " "
-         << largestTile << " " << duration;
+  << name << " " << score << " " << win << " " << moveCount << " "
+  << largestTile << " " << duration;
   newline();
   scores.close();
 }
 
 void Scoreboard::printScore(int boardsize) {
-  constexpr auto no_save_text = "No saved scores.";
-  const auto score_attributes_text = {
-      "No.", "Name", "Score", "Won?", "Moves", "Largest Tile", "Duration"};
-  //  constexpr auto num_of_score_attributes_text = 7;
-  constexpr auto header_border_text =
-      "┌─────┬────────────────────┬──────────┬──────┬───────┬──────────────┬──────────────┐";
-  constexpr auto mid_border_text =
-      "├─────┼────────────────────┼──────────┼──────┼───────┼──────────────┼──────────────┤";
-  constexpr auto bottom_border_text =
-      "└─────┴────────────────────┴──────────┴──────┴───────┴──────────────┴──────────────┘";
-  constexpr auto score_title_text = "SCOREBOARD";
-  constexpr auto divider_text = "──────────";
-  constexpr auto sp = "  ";
-  std::ostringstream str_os;
-
-  readFile(boardsize);
-
-  clearScreen();
-  drawAscii();
-  str_os << green << bold_on << sp << score_title_text << bold_off << def
-         << "\n";
-  str_os << green << bold_on << sp << divider_text << bold_off << def << "\n";
-
-  const auto number_of_scores = scoreList.size();
-  if (number_of_scores) {
-    str_os << sp << header_border_text << "\n";
-    str_os << std::left;
-    str_os << sp << "│ " << bold_on << std::begin(score_attributes_text)[0]
-           << bold_off << " │ " << bold_on << std::setw(18)
-           << std::begin(score_attributes_text)[1] << bold_off << " │ "
-           << bold_on << std::setw(8) << std::begin(score_attributes_text)[2]
-           << bold_off << " │ " << bold_on
-           << std::begin(score_attributes_text)[3] << bold_off << " │ "
-           << bold_on << std::begin(score_attributes_text)[4] << bold_off
-           << " │ " << bold_on << std::begin(score_attributes_text)[5]
-           << bold_off << " │ " << bold_on << std::setw(12)
-           << std::begin(score_attributes_text)[6] << bold_off << " │"
-           << "\n";
-    str_os << std::right;
-    str_os << sp << mid_border_text << "\n";
-
-    auto counter{1};
-    const auto print_score_stat = [&counter, &str_os](const Score i) {
-      constexpr auto number_of_fields = 7;
-      auto data_stats = std::array<std::string, number_of_fields>{};
-      data_stats = {std::to_string(counter),     i.name,
-                    std::to_string(i.score),     i.win ? "Yes" : "No",
-                    std::to_string(i.moveCount), std::to_string(i.largestTile),
-                    secondsFormat(i.duration)};
-      int hang=0, k;
-      for(k=0;k<data_stats[1].length();k++){
-          if(data_stats[1][k]<0) hang++;
-      }
-      hang = hang/3;
-      str_os << sp << "│ " << std::setw(2) << data_stats[0] << ". │ "
-             << std::left <<std::setw(18+hang) << data_stats[1] << std::right
-             << " │ " << std::setw(8) << data_stats[2] << " │ " << std::setw(4)
-             << data_stats[3] << " │ " << std::setw(5) << data_stats[4] << " │ "
-             << std::setw(12) << data_stats[5] << " │ " << std::setw(12)
-             << data_stats[6] << " │"
-             << "\n";
-      counter++;
+  if(language == 1){
+    constexpr auto header_border_text =
+    "┌─────┬────────────────────┬──────────┬──────┬───────┬──────────────┬──────────────┐";
+    constexpr auto mid_border_text =
+    "├─────┼────────────────────┼──────────┼──────┼───────┼──────────────┼──────────────┤";
+    constexpr auto bottom_border_text =
+    "└─────┴────────────────────┴──────────┴──────┴───────┴──────────────┴──────────────┘";
+    constexpr auto divider_text = "──────────";
+    constexpr auto sp = "  ";
+    constexpr auto no_save_text = "No saved scores.";
+    constexpr auto score_attributes_text = {
+      "No.", "Name", "Score", "Won?", "Moves", "Largest Tile", "Duration"
     };
+    constexpr auto score_title_text = "SCOREBOARD";
+    std::ostringstream str_os;
 
-    std::for_each(std::begin(scoreList), std::end(scoreList), print_score_stat);
-    str_os << sp << bottom_border_text << "\n";
-  } else {
-    str_os << sp << no_save_text << "\n";
+    readFile(boardsize);
+
+    clearScreen();
+    drawAscii();
+    str_os << green << bold_on << sp << score_title_text << bold_off << def
+    << "\n";
+    str_os << green << bold_on << sp << divider_text << bold_off << def << "\n";
+
+    const auto number_of_scores = scoreList.size();
+    if (number_of_scores) {
+      str_os << sp << header_border_text << "\n";
+      str_os << std::left;
+      str_os << sp << "│ " << bold_on << std::begin(score_attributes_text)[0]
+      << bold_off << " │ " << bold_on << std::setw(18)
+      << std::begin(score_attributes_text)[1] << bold_off << " │ "
+      << bold_on << std::setw(8) << std::begin(score_attributes_text)[2]
+      << bold_off << " │ " << bold_on
+      << std::begin(score_attributes_text)[3] << bold_off << " │ "
+      << bold_on << std::begin(score_attributes_text)[4] << bold_off
+      << " │ " << bold_on << std::begin(score_attributes_text)[5]
+      << bold_off << " │ " << bold_on << std::setw(12)
+      << std::begin(score_attributes_text)[6] << bold_off << " │"
+      << "\n";
+      str_os << std::right;
+      str_os << sp << mid_border_text << "\n";
+
+      auto counter{1};
+      const auto print_score_stat = [&counter, &str_os](const Score i) {
+        constexpr auto number_of_fields = 7;
+        auto data_stats = std::array<std::string, number_of_fields>{};
+        data_stats = {std::to_string(counter),     i.name,
+          std::to_string(i.score),     i.win ? "Yes" : "No",
+          std::to_string(i.moveCount), std::to_string(i.largestTile),
+          secondsFormat(i.duration)
+        };
+        int hang=0, k;
+        for(k=0;k<data_stats[1].length();k++){
+          if(data_stats[1][k]<0) hang++;
+        }
+        hang = hang/3;
+        str_os << sp << "│ " << std::setw(2) << data_stats[0] << ". │ "
+        << std::left <<std::setw(18+hang) << data_stats[1] << std::right
+        << " │ " << std::setw(8) << data_stats[2] << " │ " << std::setw(4)
+        << data_stats[3] << " │ " << std::setw(5) << data_stats[4] << " │ "
+        << std::setw(12) << data_stats[5] << " │ " << std::setw(12)
+        << data_stats[6] << " │"
+        << "\n";
+        counter++;
+      };
+
+      std::for_each(std::begin(scoreList), std::end(scoreList), print_score_stat);
+      str_os << sp << bottom_border_text << "\n";
+    } else {
+      str_os << sp << no_save_text << "\n";
+    }
+    str_os << "\n\n";
+    std::cout << str_os.str();
   }
-  str_os << "\n\n";
-  std::cout << str_os.str();
+  else{
+    constexpr auto header_border_text =
+    "┌───────┬──────────────────┬────────┬──────────┬──────────┬──────────────┬────────────┐";
+    constexpr auto mid_border_text =
+    "├───────┼──────────────────┼────────┼──────────┼──────────┼──────────────┼────────────┤";
+    constexpr auto bottom_border_text =
+    "└───────┴──────────────────┴────────┴──────────┴──────────┴──────────────┴────────────┘";
+    constexpr auto divider_text = "──────────";
+    constexpr auto sp = "  ";
+    constexpr auto no_save_text = "저장된 점수가 없습니다.";
+    constexpr auto score_attributes_text = {
+      "번호.", "이름", "점수", "성공여부", "이동횟수", "가장 큰 타일", "시간"
+    };
+    constexpr auto score_title_text = "점수판";
+    std::ostringstream str_os;
+    readFile(boardsize);
+
+    clearScreen();
+    drawAscii();
+    str_os << green << bold_on << sp << score_title_text << bold_off << def
+    << "\n";
+    str_os << green << bold_on << sp << divider_text << bold_off << def << "\n";
+
+    const auto number_of_scores = scoreList.size();
+    if (number_of_scores) {
+      str_os << sp << header_border_text << "\n";
+      str_os << std::left;
+      str_os << sp << "│ " << bold_on << std::begin(score_attributes_text)[0]
+      << bold_off << " │ " << bold_on << std::setw(18)
+      << std::begin(score_attributes_text)[1] << bold_off << " │ "
+      << bold_on << std::setw(8) << std::begin(score_attributes_text)[2]
+      << bold_off << " │ " << bold_on
+      << std::begin(score_attributes_text)[3] << bold_off << " │ "
+      << bold_on << std::begin(score_attributes_text)[4] << bold_off
+      << " │ " << bold_on << std::begin(score_attributes_text)[5]
+      << bold_off << " │ " << bold_on << std::setw(12)
+      << std::begin(score_attributes_text)[6] << bold_off << " │"
+      << "\n";
+      str_os << std::right;
+      str_os << sp << mid_border_text << "\n";
+
+      auto counter{1};
+      const auto print_score_stat = [&counter, &str_os](const Score i) {
+        constexpr auto number_of_fields = 7;
+        auto data_stats = std::array<std::string, number_of_fields>{};
+        data_stats = {std::to_string(counter),     i.name,
+          std::to_string(i.score),     i.win ? "O" : "X",
+          std::to_string(i.moveCount), std::to_string(i.largestTile),
+          secondsFormat(i.duration)
+        };
+        int hang=0, k;
+        for(k=0;k<data_stats[1].length();k++){
+          if(data_stats[1][k]<0) hang++;
+        }
+        hang = hang/3;
+        str_os << sp << "│ " << std::setw(4) << data_stats[0] << ". │ "
+        << std::left <<std::setw(16+hang) << data_stats[1] << std::right
+        << " │ " << std::setw(6) << data_stats[2] << " │ " << std::setw(8)
+        << data_stats[3] << " │ " << std::setw(8) << data_stats[4] << " │ "
+        << std::setw(12) << data_stats[5] << " │ " << std::setw(10)
+        << data_stats[6] << " │"
+        << "\n";
+        counter++;
+      };
+
+      std::for_each(std::begin(scoreList), std::end(scoreList), print_score_stat);
+      str_os << sp << bottom_border_text << "\n";
+    } else {
+      str_os << sp << no_save_text << "\n";
+    }
+    str_os << "\n\n";
+    std::cout << str_os.str();
+  }
 }
 
 void Scoreboard::printStats(int boardsize) {
-  constexpr auto stats_title_text = "STATISTICS";
-  constexpr auto divider_text = "──────────";
-  constexpr auto header_border_text = "┌────────────────────┬─────────────┐";
-  constexpr auto footer_border_text = "└────────────────────┴─────────────┘";
-  const auto stats_attributes_text = {"Best Score", "Game Count",
-                                      "Number of Wins", "Total Moves Played",
-                                      "Total Duration"};
-  constexpr auto no_save_text = "No saved statistics.";
-  constexpr auto any_key_exit_text = "Press any key to go back to menu: ";
-  constexpr auto sp = "  ";
+  if(language == 1){
+    constexpr auto stats_title_text = "STATISTICS";
+    constexpr auto divider_text = "──────────";
+    constexpr auto header_border_text = "┌────────────────────┬─────────────┐";
+    constexpr auto footer_border_text = "└────────────────────┴─────────────┘";
+    const auto stats_attributes_text = {"Best Score", "Game Count",
+    "Number of Wins", "Total Moves Played",
+    "Total Duration"};
+    constexpr auto no_save_text = "No saved statistics.";
+    constexpr auto any_key_exit_text = "Press any key to go back to menu: ";
+    constexpr auto sp = "  ";
 
-  Stats stats;
-  std::ostringstream stats_richtext;
-  if (stats.collectStatistics(boardsize)) {
-    constexpr auto num_of_stats_attributes_text = 5;
-    auto data_stats = std::array<std::string, num_of_stats_attributes_text>{};
-    data_stats = {
+    Stats stats;
+    std::ostringstream stats_richtext;
+    if (stats.collectStatistics(boardsize)) {
+      constexpr auto num_of_stats_attributes_text = 5;
+      auto data_stats = std::array<std::string, num_of_stats_attributes_text>{};
+      data_stats = {
         std::to_string(stats.bestScore), std::to_string(stats.gameCount),
         std::to_string(stats.winCount), std::to_string(stats.totalMoveCount),
         secondsFormat(stats.totalDuration)};
 
-    auto counter{0};
-    const auto populate_stats_info = [data_stats, stats_attributes_text,
-                                      &counter,
-                                      &stats_richtext](const std::string) {
-      stats_richtext << sp << "│ " << bold_on << std::left << std::setw(18)
-                     << std::begin(stats_attributes_text)[counter] << bold_off
-                     << " │ " << std::right << std::setw(11)
-                     << data_stats[counter] << " │"
-                     << "\n";
-      counter++;
-    };
+        auto counter{0};
+        const auto populate_stats_info = [data_stats, stats_attributes_text,
+        &counter,
+        &stats_richtext](const std::string) {
+          stats_richtext << sp << "│ " << bold_on << std::left << std::setw(18)
+          << std::begin(stats_attributes_text)[counter] << bold_off
+          << " │ " << std::right << std::setw(11)
+          << data_stats[counter] << " │"
+          << "\n";
+          counter++;
+        };
 
-    stats_richtext << green << bold_on << sp << stats_title_text << bold_off
-                   << def << "\n";
-    stats_richtext << green << bold_on << sp << divider_text << bold_off << def
-                   << "\n";
-    stats_richtext << sp << header_border_text << "\n";
-    std::for_each(std::begin(stats_attributes_text),
-                  std::end(stats_attributes_text), populate_stats_info);
-    stats_richtext << sp << footer_border_text << "\n";
+        stats_richtext << green << bold_on << sp << stats_title_text << bold_off
+        << def << "\n";
+        stats_richtext << green << bold_on << sp << divider_text << bold_off << def
+        << "\n";
+        stats_richtext << sp << header_border_text << "\n";
+        std::for_each(std::begin(stats_attributes_text),
+        std::end(stats_attributes_text), populate_stats_info);
+        stats_richtext << sp << footer_border_text << "\n";
 
-  } else {
-    stats_richtext << sp << no_save_text << "\n";
+    }
+    else {
+        stats_richtext << sp << no_save_text << "\n";
+    }
+
+    stats_richtext << "\n\n\n";
+    stats_richtext << sp << any_key_exit_text;
+
+    std::cout << stats_richtext.str();
+    char c;
+    std::cin >> c;
+    Menu menu;
+    menu.startMenu();
   }
+  else{
+    constexpr auto stats_title_text = "통계";
+    constexpr auto divider_text = "──────────";
+    constexpr auto header_border_text = "┌────────────────┬─────────────┐";
+    constexpr auto footer_border_text = "└────────────────┴─────────────┘";
+    const auto stats_attributes_text = {"최고 점수", "게임 횟수",
+    "성공 횟수", "전체 이동",
+    "전체 시간"};
+    constexpr auto no_save_text = "저장된 통계가 없습니다.";
+    constexpr auto any_key_exit_text = "키보드를 입력하면 메뉴로 돌아갑니다.: ";
+    constexpr auto sp = "  ";
 
-  stats_richtext << "\n\n\n";
-  stats_richtext << sp << any_key_exit_text;
+    Stats stats;
+    std::ostringstream stats_richtext;
+    if (stats.collectStatistics(boardsize)) {
+      constexpr auto num_of_stats_attributes_text = 5;
+      auto data_stats = std::array<std::string, num_of_stats_attributes_text>{};
+      data_stats = {
+        std::to_string(stats.bestScore), std::to_string(stats.gameCount),
+        std::to_string(stats.winCount), std::to_string(stats.totalMoveCount),
+        secondsFormat(stats.totalDuration)};
 
-  std::cout << stats_richtext.str();
-  char c;
-  std::cin >> c;
-  Menu menu;
-  menu.startMenu();
+        auto counter{0};
+        const auto populate_stats_info = [data_stats, stats_attributes_text,
+        &counter,
+        &stats_richtext](const std::string) {
+          stats_richtext << sp << "│ " << bold_on << std::left << std::setw(18)
+          << std::begin(stats_attributes_text)[counter] << bold_off
+          << " │ " << std::right << std::setw(11)
+          << data_stats[counter] << " │"
+          << "\n";
+          counter++;
+        };
+
+        stats_richtext << green << bold_on << sp << stats_title_text << bold_off
+        << def << "\n";
+        stats_richtext << green << bold_on << sp << divider_text << bold_off << def
+        << "\n";
+        stats_richtext << sp << header_border_text << "\n";
+        std::for_each(std::begin(stats_attributes_text),
+        std::end(stats_attributes_text), populate_stats_info);
+        stats_richtext << sp << footer_border_text << "\n";
+
+    }
+    else {
+        stats_richtext << sp << no_save_text << "\n";
+    }
+
+    stats_richtext << "\n\n\n";
+    stats_richtext << sp << any_key_exit_text;
+
+    std::cout << stats_richtext.str();
+    char c;
+    std::cin >> c;
+    Menu menu;
+    menu.startMenu();
+  }
 }
 
 void Scoreboard::padding(std::string name) {
@@ -181,8 +325,7 @@ void Scoreboard::readFile(int size) {
   long long moveCount;
   double duration;
 
-  while (scores >> playerName >> playerScore >> win >> moveCount >>
-         largestTile >> duration) {
+  while (scores >> playerName >> playerScore >> win >> moveCount >> largestTile >> duration) {
 
     Score bufferScore;
     bufferScore.name = playerName;
@@ -203,10 +346,18 @@ void Scoreboard::readFile(int size) {
 
 void Scoreboard::save() {
   constexpr auto score_saved_text = "Score saved!";
+  constexpr auto score_saved_text_kor = "점수가 저장되었습니다!";
   constexpr auto sp = "  ";
   std::ostringstream score_saved_richtext;
-  score_saved_richtext << green << bold_on << sp << score_saved_text << bold_off
-                       << def << "\n";
+  if(language == 1){
+    score_saved_richtext << green << bold_on << sp << score_saved_text << bold_off
+    << def << "\n";
+  }
+  else{
+    score_saved_richtext << green << bold_on << sp << score_saved_text_kor << bold_off
+    << def << "\n";
+  }
+
 
   prompt();
   writeToFile();
